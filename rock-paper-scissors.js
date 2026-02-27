a8f83ab468a16f08fb531310ba6fce8330186641
readers02 <scott.reader@rogers.com>
2026-02-27 15:50:37 -0500

    refactor code to align with guidelines

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,66 @@
 
 
-//take choice
-let playerChoice = parseInt(prompt("Please choose a number between 1 and 3 (1:rock, 2:paper, 3:scissors"));
-let comChoice = Math.floor(Math.random()*3) + 1;
-
-if(playerChoice === comChoice){
-    console.log("You both picked the same, it's a tie!")
-}else if((playerChoice === 1 && comChoice === 3) || (playerChoice === 2 && comChoice === 1) || (playerChoice === 3 && comChoice === 2)){
-    console.log("You win!");
-}else if((comChoice === 1 && playerChoice === 3) || (comChoice === 2 && playerChoice === 1) || (comChoice === 3 && playerChoice === 2)){
-
-}else{
-    console.log("Something went wrong")
+function getComputerChoice(){
+    let comChoice = Math.floor(Math.random()*3) + 1;
+    if(comChoice===1){
+        return "Rock";
+    }else if(comChoice===2){
+        return "Paper";
+    }else if(comChoice===3){
+        return "Scissors";
+    }else{
+        return null;
+    }
 }
+
+function getHumanChoice(){
+    let playerChoice = parseInt(prompt("Please choose a number between 1 and 3 (1:rock, 2:paper, 3:scissors"));
+        if(playerChoice===1){
+        return "Rock";
+    }else if(playerChoice===2){
+        return "Paper";
+    }else if(playerChoice===3){
+        return "Scissors";
+    }else{
+        return null;
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log("You both picked the same, it's a tie!")
+    }else if((humanChoice === "Rock" && computerChoice === "Scissors") || (humanChoice === "Paper" && computerChoice === "Rock") || (humanChoice === "Scissors" && computerChoice === "Paper")){
+        console.log("You win!");
+        humanScore++
+    }else if((computerChoice === "Rock" && humanChoice === "Scissors") || (computerChoice === "Paper" && humanChoice === "Rock") || (computerChoice === "Scissors" && humanChoice === "Paper")){
+        console.log("You Lose.");
+        computerScore++;
+    }else{
+        console.log("Something went wrong");
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playGame(){
+    humanScore = 0;
+    computerScore = 0;
+    for(let i = 0; i<5; i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection,computerSelection)
+    }
+
+    if(humanScore>computerScore){
+        console.log("You won the game!")
+    }else if(humanScore<computerScore){
+        console.log("You lost the game!")
+    }else{
+        console.log("something went wrong.")
+    }
+}
+
+playGame();
